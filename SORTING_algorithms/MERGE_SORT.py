6d95a5f46af8c4b3e3dfccdcc9fe639890db956c
sujay752000 <sujayprasad010@gmail.com>
2024-07-13 22:04:56 +0530

    Merge Sort  Implemented

--- SORTING_algorithms/MERGE_SORT.py
+++ SORTING_algorithms/MERGE_SORT.py
@@ -1,5 +1,5 @@
 def merge(arr, low, mid, high):
-    temp =[]
+    temp = []
     i = low
     j = mid + 1
 
@@ -12,6 +12,7 @@ def merge(arr, low, mid, high):
             temp.append(arr[j])
             j += 1
 
+
     while i <= mid:
         temp.append(arr[i])
         i += 1
@@ -20,22 +21,18 @@ def merge(arr, low, mid, high):
         temp.append(arr[j])
         j += 1
 
+    
     for i in range(0, len(temp)):
         arr[low + i] = temp[i]
+
+
 
 
 def merge_Sort(arr, low, high):
 
     if low < high:
-
         mid = (low + high) // 2
+
         merge_Sort(arr, low, mid)
         merge_Sort(arr, mid + 1, high)
         merge(arr, low, mid, high)
-        
-
-
-# lst = [3, 1, 2, 2, 1, 5, 4, 6, 4]
-lst = [12, 3, 6, 5, 1, 4]
-merge_Sort(lst, 0, len(lst) - 1)
-print(lst)
